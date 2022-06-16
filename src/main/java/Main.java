ee1ff5b7d14ed51987c34a40b60210967d69e0b9
MajaMaric123 <maja.maric2510@gmail.com>
2022-06-16 23:05:47 +0200

    New

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class Domaci1 {
+public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Petar\\Downloads\\chromedriver_win32(1)\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
